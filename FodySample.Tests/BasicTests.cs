5c03da330ea7a3cfe80273be50bc4d25fbe67703
Rob Rodi <robert.rodi@gmail.com>
2013-01-29 08:00:22 -0800

    Added Timer, Test logs time.

---
+++ FodySample.Tests/BasicTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FodySample.Tests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void Bar()
+        {
+            new Foo().Bar();
+        }
+    }
+}
---
+++ FodySample/Foo.cs
@@ -0,0 +1,19 @@
+﻿using MethodTimer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FodySample
+{
+    public class Foo
+    {
+        [Time]
+        public void Bar()
+        {
+            Thread.Sleep(1234);
+        }
+    }
+}
