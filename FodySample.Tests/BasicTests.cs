86b2c3dd572d6fb31076589b528d612f23d045af
Rob Rodi <robert.rodi@gmail.com>
2013-01-29 09:02:55 -0800

    Testing logging & asynchronous. Note: Test fails for async.

--- FodySample.Tests/BasicTests.cs
+++ FodySample.Tests/BasicTests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using FluentAssertions;
 
 namespace FodySample.Tests
 {
@@ -13,6 +15,22 @@ namespace FodySample.Tests
         public void Bar()
         {
             new Foo().Bar();
+        }
+
+        [Fact]
+        public void Bar_async()
+        {
+            Stopwatch s = Stopwatch.StartNew();
+            var foo = new Foo();
+            var task = foo.Bar_Async();
+            task.Wait();
+
+            MethodTimeLogger.numCalls.Should().Be(1, "there was one call to the method, this may mean the state machine is logging when we wait");
+
+            ///s.ElapsedMilliseconds
+            long min = ((long)(s.ElapsedMilliseconds * .8));
+            long max = ((long)(s.ElapsedMilliseconds * 1.2));
+            MethodTimeLogger.lastCount.Should().BeInRange(min, max, "the timer should reflect the actual time, not when the async promise is returned");
         }
     }
 }
--- FodySample/Foo.cs
+++ FodySample/Foo.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,7 +15,33 @@ namespace FodySample
         [Time]
         public void Bar()
         {
-            Thread.Sleep(1234);
+            Thread.Sleep(234);
+        }
+        [Time]
+        public async Task Bar_Async()
+        {
+            var length = await ExampleMethodAsync();
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXX: {0}", length);
+        }
+
+        // Thx MSDN: http://msdn.microsoft.com/en-us/library/hh156513(v=VS.110)
+        public async Task<int> ExampleMethodAsync()
+        {
+            return (await new HttpClient().GetStringAsync("http://msdn.microsoft.com")).Length;
+        }
+    }
+
+    // Interceptor.  Dunno how this works just yet, but it does :)
+    public static class MethodTimeLogger
+    {
+        public static long lastCount;
+        public static ushort numCalls;
+        public static void Log(MethodBase methodBase, long milliseconds)
+        {
+            Console.WriteLine("RR {0}.{1} : {2}", methodBase.DeclaringType.Name, methodBase.Name, milliseconds);
+            numCalls++;
+            if (milliseconds > 0) // dunno why this happens either.
+                lastCount = milliseconds;
         }
     }
 }
